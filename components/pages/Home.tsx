cbb2cb91c496d7e43622e629f9a7cdb3334511e6
Anna <admin@lensesrequired.com>
2025-08-14 06:04:51 -0600

    Refactor view switch

--- components/pages/Home.tsx
+++ components/pages/Home.tsx
@@ -3,18 +3,36 @@
 import { Overview } from '@/components/home/Overview';
 import { Today } from '@/components/home/Today';
 import { Box, SegmentGroup } from '@chakra-ui/react';
+import { useSearchParams } from 'next/navigation';
 import { useState } from 'react';
 
-export const Home = () => {
-  const [value, setValue] = useState<string | null>('Overview');
+enum View {
+  OVERVIEW = 'overview',
+  TODAY = 'today',
+}
+
+export const Home = ({}) => {
+  const searchParams = useSearchParams();
+  const view = searchParams.get('view');
+  const [value, setValue] = useState<View | null>(
+    (view as View) || View.OVERVIEW,
+  );
+
   return (
     <Box display="flex" alignItems="center" flexDirection="column" p={3}>
-      <SegmentGroup.Root value={value} onValueChange={(e) => setValue(e.value)}>
+      <SegmentGroup.Root
+        value={value}
+        onValueChange={(e) => setValue((e.value || '').toLowerCase() as View)}
+      >
         <SegmentGroup.Indicator />
-        <SegmentGroup.Items items={['Overview', 'Today']} />
+        <SegmentGroup.Items
+          items={Object.values(View).map((view) =>
+            view.replace(/\b\w/g, (char) => char.toUpperCase()),
+          )}
+        />
       </SegmentGroup.Root>
-      {value === 'Overview' && <Overview />}
-      {value === 'Today' && <Today />}
+      {value === View.OVERVIEW && <Overview />}
+      {value === View.TODAY && <Today />}
     </Box>
   );
 };
